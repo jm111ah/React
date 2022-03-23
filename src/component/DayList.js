6b28988547c23e4337bd0ae57b2d3cc9ad8bf5b5
tacoyaggi93 <tacoyaggi93@gmail.com>
2022-03-24 00:55:46 +0900

    1111111111

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
 import dummy from "../db/data.json"
 
--- src/component/Word.js
+++ src/component/Word.js
@@ -19,13 +19,6 @@ export default function Word({word}){
         <tr className={isDone ? "off" : ""}>
         <td><input type="checkbox" checked={isDone} onChange={toggleDone}/></td>
         <td>{word.kor}</td>
-        {/* <td>{isShow && word.kor}</td>
-        <td>
-            <button onClick={toggleShow}>
-            뜻 {isShow ? "숨기기" : "보기"}
-            </button>
-            <button className="btn_del">삭제</button>
-        </td> */}
     </tr>
     );
 }
